c3e00c122c5dfdade4ea136e33c8ee583d383f5a
Your Name <your.email@example.com>
2024-04-11 16:06:15 +0200

    Update

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
+    path('formulaire/', views.data),
+    path('bonjour/', views.bonjour),
 ]
--- firstproject/myfirstapp/views.py
+++ firstproject/myfirstapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'myfirstapp/index.html')
+
+def data(request):
+    """Affiche un formulaire de saisie de nom."""
+    return render(request, 'myfirstapp/formulaire.html')
+
+def bonjour(request):
+    """Récupère le nom saisi dans le formulaire et l'affiche dans une page de salutation."""
+    nom=request.GET['nom']
+    return render(request, 'myfirstapp/bonjour.html', {'nom': nom})
